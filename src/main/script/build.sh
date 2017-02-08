9b88640ad0f159561457156823ee5440255f900f
Twan Goosen <twan.goosen@mpi.nl>
2017-02-08 17:47:10 +0100

    Change of strategy: keep file names but modify Gruntfile on the fly

--- src/main/script/build.sh
+++ src/main/script/build.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 set -e
 
-#gcp and gmv can be installed on MacOS via brew. Run "brew install coreutils" to do so.
+#gcp can be installed on MacOS via brew. Run "brew install coreutils" to do so.
 CP=`which gcp||which cp` #if gcp available (on Mac), use it instead of BSD cp
-MV=`which gmv||which mv`  #if gmv available (on Mac), use it instead of BSD mv
 
 npm set progress='false'
 
@@ -20,13 +19,14 @@ curl --fail --location --proto '=https' --silent --show-error --tlsv1 \
 ## Apply CLARIN base style.
 echo "Applying CLARIN flavour..."
 cd -- "bootstrap-${BOOTSTRAP_VER}/"
-### Safe original LESS root
-${MV} -- 'less/bootstrap.less' 'less/bootstrap-original.less'
 ### Put CLARIN root wrapper in place
-${CP} -f -- '../src/main/less/clarin-bootstrap.less' 'less/bootstrap.less'
+${CP} -f -- '../src/main/less/clarin-bootstrap.less' 'less/clarin-bootstrap.less'
 ### CLARIN customisations
 ${CP} -f -- '../src/main/less/clarin-additions.less' 'less/clarin-additions.less'
 ${CP} -f -- '../src/main/less/variables.less' 'less/variables.less'
+
+### Adapt Gruntfile to use CLARIN root wrapper
+sed -i -e "s/'less\\/bootstrap\\.less'/'less\\/clarin-bootstrap.less'/g" Gruntfile.js
 
 ## Build bootstrap
 echo "Building CLARIN bootstrap distribution..."
